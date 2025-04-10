a141822ae718b7ff65c1a4a65cd41f865ec1cc44
Giovanna Rigo <giovannarigoph@gmail.com>
2025-04-10 12:39:12 -0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import LogoComponent from './LogoComponent/LogoComponent'
+
+function App() {
+
+  return (
+    <>
+      <div id='backgroudSearch'>
+      <LogoComponent></LogoComponent>
+
+      </div>
+    </>
+  )
+}
+
+export default App
